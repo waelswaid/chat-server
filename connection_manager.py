1cdc12d1201b946f58708b5cfa99f0247eef71c3
waelswaid <waelswaid@gmail.com>
2026-03-24 22:26:53 +0000

    finished core connection loop

--- connection_manager.py
+++ connection_manager.py
@@ -1,9 +1,9 @@
 from fastapi import WebSocket
-
+from schemas.message import Message
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: dict[int, WebSocket]
+        self.active_connections: dict[int, WebSocket] = {}
 
     async def connect(self, websocket: WebSocket, client_id:int):
         await websocket.accept()
@@ -12,9 +12,10 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket, client_id: int):
         del self.active_connections[client_id]
 
-    async def send_personal_message(self, message: str, client_id: int):
-        websocket = self.active_connections.pop(client_id)
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: Message):
+        to_websocket = self.active_connections.get(message.to)
+        if to_websocket:
+            await to_websocket.send_text(message.message)
 
     async def broadcast(self, message: str):
         ws_values = self.active_connections.values()
--- ws_router.py
+++ ws_router.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from connection_manager import manager
-
+from schemas.message import Message
 
 websocket_router = APIRouter()
 
@@ -9,11 +9,13 @@ websocket_router = APIRouter()
 @websocket_router.websocket("/ws/{client_id}")
 async def route_to_server(websocket: WebSocket, client_id: int):
     await manager.connect(websocket, client_id)
+    await manager.broadcast(f"user {client_id} has joined the chat.")
     try:
         while True:
             data = await websocket.receive_json()
-            recipient_id = data["to"]
-            message = data["message"]
-            pass
-    except:
-        pass
+            message = Message(**data)
+            await manager.send_personal_message(message)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, client_id)
+        await manager.broadcast(f"user {client_id} has left the chat.")
