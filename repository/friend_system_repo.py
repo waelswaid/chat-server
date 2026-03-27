a483bd87d5df5600908ef7260b170d2ca535c453
waelswaid <waelswaid@gmail.com>
2026-03-27 03:40:47 +0300

    more request_handler work: finished friend_remove in service layer finished friend_remove_from_db in repo layer

--- repository/friend_system_repo.py
+++ repository/friend_system_repo.py
@@ -4,7 +4,7 @@ from schemas.friend_request import FriendRequest, FriendAccept,FriendDecline, Fr
 from database import async_session
 from datetime import datetime, timezone
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import delete
+from sqlalchemy import delete, or_, and_
 
 async def send_friend_req_to_db(session, to:str, user_id: str) -> None:
     request = PendingRequests(
@@ -16,10 +16,10 @@ async def send_friend_req_to_db(session, to:str, user_id: str) -> None:
     return
 
 # delete from PendingRequests, insert into Friendships (single transaction)
-async def friend_request_accept_to_db(session, from_user_id:str, accepter_id:str) -> None:
+async def friend_request_accept_to_db(session, requester_id:str, accepter_id:str) -> None:
     result = await session.execute(
         delete(PendingRequests).where(
-            PendingRequests.sender_id == from_user_id,
+            PendingRequests.sender_id == requester_id,
             PendingRequests.receiver_id == accepter_id
         )
     )
@@ -27,9 +27,36 @@ async def friend_request_accept_to_db(session, from_user_id:str, accepter_id:str
         raise ValueError("no pending request found")
     
     session.add_all([
-        Friendships(user_id=accepter_id, friend_id=from_user_id),
-        Friendships(user_id = from_user_id, friend_id=accepter_id)
+        Friendships(user_id=accepter_id, friend_id=requester_id),
+        Friendships(user_id = requester_id, friend_id=accepter_id)
     ])
     await session.commit()
     return
         
+
+async def friend_req_decline_to_db(session, requester_id: str, decliner_id:str) -> None:
+    result = await session.execute(
+        delete(PendingRequests).where(
+            PendingRequests.sender_id == requester_id,
+            PendingRequests.receiver_id == decliner_id
+        )
+    )
+    if result.rowcount == 0:
+        raise ValueError("no pending request found")
+    await session.commit()
+    return
+
+async def friend_remove_from_db(session, removed_id: str, remover_id:str) -> None:
+    result = await session.execute(
+        delete(Friendships).where(
+            or_(
+                and_(Friendships.user_id == remover_id, Friendships.friend_id == removed_id),
+                and_(Friendships.user_id == removed_id, Friendships.friend_id == remover_id)
+            )
+
+        )
+    )
+    if result.rowcount == 0:
+        raise ValueError("not friends")
+    await session.commit()
+    return
--- services/friend_service.py
+++ services/friend_service.py
@@ -2,7 +2,8 @@ from schemas.friend_request import FriendRequest, FriendAccept,FriendDecline, Fr
 from fastapi import WebSocket
 from database import async_session
 from repository.friend_system_repo import (
-    send_friend_req_to_db, friend_request_accept_to_db
+    send_friend_req_to_db, friend_request_accept_to_db,
+    friend_req_decline_to_db, friend_remove_from_db
 ) 
 from sqlalchemy.exc import IntegrityError
 from connection_manager import manager
@@ -24,11 +25,11 @@ async def send_friend_request(req : FriendRequest, websocket:WebSocket, user_id:
     
 
 
-async def friend_request_accept(req : FriendAccept, websocket:WebSocket, sender_id: str) -> None:
-    from_user_id = req.from_user
+async def friend_request_accept(req : FriendAccept, websocket:WebSocket, accepter_id: str) -> None:
+    requester_id = req.from_user
     async with async_session() as session:
         try:
-            await friend_request_accept_to_db(session, from_user_id, sender_id)
+            await friend_request_accept_to_db(session, requester_id, accepter_id)
         except (IntegrityError, ValueError):
             await session.rollback()
             await websocket.send_json({"type":"friend_req_accept_error", "message":"invalid request"})
@@ -37,8 +38,30 @@ async def friend_request_accept(req : FriendAccept, websocket:WebSocket, sender_
         return
     
 
-async def friend_request_declined(req : FriendDecline, websocket:WebSocket, user_id: str):
-    pass
+async def friend_request_declined(req : FriendDecline, websocket:WebSocket, decliner_id: str) -> None:
+    requester_id = req.from_user
+    async with async_session() as session:
+        try:
+            await friend_req_decline_to_db(session, requester_id, decliner_id)
+        except(IntegrityError, ValueError):
+            await session.rollback()
+            await websocket.send_json({"type":"friend_req_decline_error", "message":"invalid request"})
+            return
+        await websocket.send_json({"type": "friend_req_declined", "from": req.from_user})
+        return
+        
+async def friend_remove(req: FriendRemove, websocket : WebSocket, remover_id: str):
+    removed_id = req.user_id
+    async with async_session() as session:
+        try:
+            await friend_remove_from_db(session, removed_id,remover_id)
+        except (IntegrityError, ValueError):
+            await session.rollback()
+            await websocket.send_json({"type":"friend_remove_error", "message":"invalid request"})
+            return
+        await websocket.send_json({"type": "friend_remove", "from": req.user_id})
+        return
+        
 
 async def return_friend_list(websocket:WebSocket):
     pass
@@ -58,6 +81,8 @@ async def request_handler(msg_type:str, data:dict, websocket:WebSocket, user_id:
     elif msg_type == "friend_decline":
         req = FriendDecline(**data)
         await friend_request_declined(req,websocket,user_id)
+    elif msg_type == "friend_remove":
+        req = FriendRemove(**data)
     elif msg_type == "friend_list":
         await return_friend_list(websocket)
     elif msg_type == "pending_list":
