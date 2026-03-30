8ba76786d1ec33165b28bab1e4e63b68c6bc3a17
waelswaid <waelswaid@gmail.com>
2026-03-30 03:08:23 +0300

    added redis rate limit on upload endpoint

--- core/config.py
+++ core/config.py
@@ -8,6 +8,9 @@ class Settings(BaseSettings):
     S3_BUCKET_NAME : str = ""
     AWS_REGION : str = "us-east-1"
     CDN_DOMAIN : str = ""
+    REDIS_URL: str = ""
+    UPLOAD_LIMIT_TTL: int = 60 * 30 #30 mins
+    UPLOAD_RATE_LIMIT: int = 10
 
 
 settings = Settings()
--- routes/upload_route.py
+++ routes/upload_route.py
@@ -1,16 +1,27 @@
-from fastapi import UploadFile, File, APIRouter, Depends
+from fastapi import UploadFile, File, APIRouter, Depends, HTTPException
 from services.upload_service import upload_file
 from core.auth_token import validate_token
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import redis.asyncio as redis
+from core.config import settings
 
 security = HTTPBearer() # handles http header parsing
 upload_router = APIRouter()
 
+# parses the url (host="redis", port=6379, db=1)
+redis_client = redis.from_url(settings.REDIS_URL)
 
 
 @upload_router.post("/upload/")
 async def upload_route(credentials: HTTPAuthorizationCredentials = Depends(security), file: UploadFile = File(...)):
     sender_id, sender_email = validate_token(credentials.credentials)
+    redis_key = f"rate:{sender_email}"
+    # incr returns value=1 on non-existent key
+    count = await redis_client.incr(redis_key)
+    if count == 1:# new key-> set ttl with .expire()
+        await redis_client.expire(redis_key, settings.UPLOAD_LIMIT_TTL)
+    if count > settings.UPLOAD_RATE_LIMIT:
+            raise HTTPException(status_code=429, detail="too many attempts, try again later")
     return await upload_file(sender_id, file)
 
 
